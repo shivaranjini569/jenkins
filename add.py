8b03e62f4d2a5303e3d92891d24880b9898f7db4
shivaranjini569 <shivaranjini.r2024@vitstudent.ac.in>
2026-09-23 10:31:50 +0530

    add.py

---
+++ add.py
@@ -0,0 +1,17 @@
+import sys
+
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    num1 = int(sys.argv[1])
+    num2 = int(sys.argv[2])
+
+    result = add_numbers(num1, num2)
+
+    print("=================================")
+    print("Addition Result")
+    print("=================================")
+    print(f"First Number : {num1}")
+    print(f"Second Number: {num2}")
+    print(f"Sum : {result}")
